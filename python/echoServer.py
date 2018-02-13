ede70cedcba29c93c6040bca5b288ce53c1ee7e7
Juuso Metsävuori <juuso.metsavuori@gmail.com>
2018-02-13 21:54:45 +0200

    added echo callback for echo...

--- python/echoServer.py
+++ python/echoServer.py
@@ -7,7 +7,9 @@ def addClient(client, server):
     print('newClient')
 
 def echo(client, server, message):
+    print('got message: ' + str(message))
     server.send_message(client, message)
 
 server.set_fn_new_client(addClient)
+server.set_fn_message_received(echo)
 server.run_forever()
